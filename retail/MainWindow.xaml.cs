8b42637d98ac3120c32fdabd68ddfdf3054dceb9
nuhtina <AlinkaN2005@mail.ru>
2025-06-11 13:03:02 +0300

    11.06

--- retail/MainWindow.xaml.cs
+++ retail/MainWindow.xaml.cs
@@ -24,32 +24,5 @@ namespace retail
         {
             InitializeComponent();
         }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                var userObj = ApplicationData.AppConnect.model01.Users.FirstOrDefault(x => x.email == TBemail.Text && x.Password == PBpassword.Password);
-                if (userObj == null)
-                {
-                    MessageBox.Show("Такого пользователя нет", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-                else
-                {
-                    MessageBox.Show("Здравствуйте", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
-                    AppConnect.AuthorID = userObj.AuthorID;
-                    NavigationService.Navigate(new DataOutput());
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ошибка" + ex.Message.ToString(), "Критическая ошибка приложения", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-        }
-
-        private void ButtonRegistr_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService.Navigate(new Registration());
-        }
     }
 }
